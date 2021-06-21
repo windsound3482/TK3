4991f4b84ed77a7c03ebaa7c8d293214c1abe638
Charles Liao <jilin.liao@stud.tu-darmstadt.de>
2021-06-21 15:01:42 +0200

    POI

--- app/src/main/java/com/example/tk3/MainActivity.java
+++ app/src/main/java/com/example/tk3/MainActivity.java
@@ -7,17 +7,90 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Marker.OnMarkerDragListener;
+import org.osmdroid.views.overlay.Polyline;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.StrictMode;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.clustering.RadiusMarkerClusterer;
+import org.osmdroid.bonuspack.clustering.StaticCluster;
+import org.osmdroid.bonuspack.kml.KmlDocument;
+import org.osmdroid.bonuspack.kml.KmlFeature;
+import org.osmdroid.bonuspack.kml.KmlFolder;
+import org.osmdroid.bonuspack.kml.KmlLineString;
+import org.osmdroid.bonuspack.kml.KmlPlacemark;
+import org.osmdroid.bonuspack.kml.KmlPoint;
+import org.osmdroid.bonuspack.kml.KmlPolygon;
+import org.osmdroid.bonuspack.kml.KmlTrack;
+import org.osmdroid.bonuspack.kml.Style;
+import org.osmdroid.bonuspack.location.NominatimPOIProvider;
+import org.osmdroid.bonuspack.location.OverpassAPIProvider;
+import org.osmdroid.bonuspack.location.POI;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
+import org.osmdroid.bonuspack.routing.RoadNode;
+import org.osmdroid.bonuspack.utils.BonusPackHelper;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.util.BoundingBox;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.FolderOverlay;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Marker.OnMarkerDragListener;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.Polygon;
+import org.osmdroid.views.overlay.Polyline;
+import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
+import org.osmdroid.views.overlay.infowindow.InfoWindow;
+import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +98,6 @@ public class MainActivity extends AppCompatActivity {
     private MapView map = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
@@ -39,25 +111,82 @@ public class MainActivity extends AppCompatActivity {
         //tile servers will get you banned based on this string
 
         //inflate and create the map
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        //Introduction
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().setUserAgentValue("OBP_Tuto/1.0");
         setContentView(R.layout.activity_main);
-
         map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-
-        requestPermissionsIfNecessary(new String[] {
-                // if you need to show the current location, uncomment the line below
-                // Manifest.permission.ACCESS_FINE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        });
-
-        map.setBuiltInZoomControls(true);
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
         map.setMultiTouchControls(true);
+        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
         IMapController mapController = map.getController();
-        mapController.setZoom(9.5);
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setZoom(10.0);
         mapController.setCenter(startPoint);
+
+        //0. Using the Marker overlay
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker.setTitle("Start point");
+        //startMarker.setIcon(getResources().getDrawable(R.drawable.marker_kml_point).mutate());
+        //startMarker.setImage(getResources().getDrawable(R.drawable.ic_launcher));
+        //startMarker.setInfoWindow(new MarkerInfoWindow(R.layout.bonuspack_bubble_black, map));
+        startMarker.setDraggable(true);
+        startMarker.setOnMarkerDragListener(new OnMarkerDragListenerDrawer());
+        map.getOverlays().add(startMarker);
+
+        NominatimPOIProvider poiProvider = new NominatimPOIProvider("OSMBonusPackTutoUserAgent");
+        ArrayList<POI> pois = poiProvider.getPOICloseTo(startPoint,"cinema" , 50, 0.1);
+        FolderOverlay poiMarkers = new FolderOverlay(this);
+        map.getOverlays().add(poiMarkers);
+
+        Drawable poiIcon = getResources().getDrawable(R.drawable.marker_poi_default);
+        for (POI poi:pois){
+            Marker poiMarker = new Marker(map);
+            poiMarker.setTitle(poi.mType);
+            poiMarker.setSnippet(poi.mDescription);
+            poiMarker.setPosition(poi.mLocation);
+            poiMarker.setIcon(poiIcon);
+            if (poi.mThumbnail != null){
+                poiMarker.setImage(new BitmapDrawable(poi.mThumbnail));
+            }
+            poiMarkers.add(poiMarker);
+        }
+
     }
+    class OnMarkerDragListenerDrawer implements OnMarkerDragListener {
+        ArrayList<GeoPoint> mTrace;
+        Polyline mPolyline;
+
+        OnMarkerDragListenerDrawer() {
+            mTrace = new ArrayList<GeoPoint>(100);
+            mPolyline = new Polyline();
+            mPolyline.setColor(0xAA0000FF);
+            mPolyline.setWidth(2.0f);
+            mPolyline.setGeodesic(true);
+            map.getOverlays().add(mPolyline);
+        }
+
+        @Override public void onMarkerDrag(Marker marker) {
+            //mTrace.add(marker.getPosition());
+        }
+
+        @Override public void onMarkerDragEnd(Marker marker) {
+            mTrace.add(marker.getPosition());
+            mPolyline.setPoints(mTrace);
+            map.invalidate();
+        }
+
+        @Override public void onMarkerDragStart(Marker marker) {
+            //mTrace.add(marker.getPosition());
+        }
+    }
+
 
     @Override
     public void onResume() {
